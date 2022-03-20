e20d521640978b4f5efbe4298f5a8f85528dcf0e
ashliecodes <65076082+ashliecodes@users.noreply.github.com>
2022-03-20 12:41:39 +0000

    installed sass

---
+++ pages/About/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1> about:</h1>
+      <p>
+        Hi there, my name is Ashlie! After working in the legal industry as a
+        legal assistant, I decided that law wasn&apos;t for me. I found it very
+        boring and tedious and became underwhelmed quickly.
+      </p>
+      <br />
+      <p>
+        I am fun and creative (at least I like to think so), so I wanted
+        something that matched this - something that challendged me whilst also
+        being fun and creative.
+      </p>
+      <br />
+      <p>Software development struck that balance for me!</p>
+      <br />
+      <p>
+        I did the Code First Girls Introduction into Web Development course and
+        Introduction into Python Programming course which was my first taste of
+        coding and I wanted more (greedy, I know)!
+      </p>
+      <br />
+      <p>
+        This lead me to join the School of Code where I spent the last couple
+        months on the bootcamp learning to code in order to finally kickstart my
+        long awaited career in software development.
+      </p>
+    </div>
+  );
+}
+
+export default About;
