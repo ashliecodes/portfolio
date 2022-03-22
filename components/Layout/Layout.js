defb05880c6706385ac0e9406d8bba705a51ac65
ashliecodes <65076082+ashliecodes@users.noreply.github.com>
2022-03-22 15:10:05 +0000

    fixed the projects page

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,7 +1,11 @@
-import React from "react";
-
-function Layout() {
-  return <div>Layout</div>;
+import Navbar from "../Navbar/Navbar";
+function Layout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
 
 export default Layout;
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,7 +1,29 @@
-import React from "react";
+import Link from "next/link";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <Link href={"/"}>
+            <a>
+              <h1>&lt;ashliecodes/&gt;</h1>
+            </a>
+          </Link>
+        </div>
+
+        <Link href={"/about"}>
+          <a>about</a>
+        </Link>
+        <Link href={"/projects"}>
+          <a>projects</a>
+        </Link>
+        <Link href={"/contact"}>
+          <a>contact</a>
+        </Link>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
