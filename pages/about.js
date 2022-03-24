b00cb1efec4269265858bcb56cafa5eadcd87204
ashliecodes <65076082+ashliecodes@users.noreply.github.com>
2022-03-24 12:45:57 +0000

    styling projects page and about page

--- pages/about.js
+++ pages/about.js
@@ -5,42 +5,36 @@ import portfolioPic from "../images/portfolioPic.jpg";
 function About() {
   return (
     <div className={styles.container}>
-      <h1> about:</h1>
-      <p className={styles.about}>
-        Hi there, my name is Ashlie! After working in the legal industry as a
-        legal assistant, I decided that law wasn&apos;t for me. I felt
-        unstimulated and became underwhelmed quickly.
-      </p>
-      <br />
-      <p className={styles.about}>
-        I am fun and creative (at least I like to think so), so I wanted
-        something that matched this - something that challendged me whilst also
-        being fun and creative.
-      </p>
-      <br />
-      <p className={styles.about}>
-        Software development struck that balance for me!
-      </p>
-      <br />
-      <p className={styles.about}>
-        I did the Code First Girls Introduction into Web Development course and
-        Introduction into Python Programming course which was my first taste of
-        coding and I wanted more... (greedy, I know)!
-      </p>
-      <br />
-      <p className={styles.about}>
-        This led me to join the School of Code where I spent the last couple
-        months on the bootcamp learning to code in order to finally kickstart my
-        long awaited career in software development.
-      </p>
-      <div className={styles.imgContainer}>
-        <Image
-          className={styles.img}
-          src={portfolioPic}
-          height="300px"
-          width="310px"
-          alt="a picture of ashlie"
-        />
+      <div className={styles.main}>
+        <div className={styles.aboutDescription}>
+          <h2>about:</h2>
+          <p className={styles.paragraph}>
+            Hi there, my name is Ashlie! After working in the legal industry as
+            a legal assistant, I decided that law wasn&apos;t for me. I felt
+            unstimulated and became underwhelmed quickly. I am fun and creative
+            (at least I like to think so), so I wanted something that matched
+            this - something that challendged me whilst also being fun and
+            creative.
+            <br />
+            <br />
+            Software development struck that balance for me!
+            <br />
+            <br />I did the Code First Girls Introduction into Web Development
+            course and Introduction into Python Programming course which was my
+            first taste of This led me to join the School of Code where I spent
+            the last couple months on the bootcamp learning to code in order to
+            finally kickstart my long awaited career in software development.
+          </p>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src={portfolioPic}
+            height="300px"
+            width="310px"
+            alt="a picture of ashlie"
+          />
+        </div>
       </div>
     </div>
   );
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
-import React from "react";
 import styles from "../styles/projects.module.scss";
+import Image from "next/image";
+import raduno from "../images/raduno.png";
 
 function Projects() {
   return (
@@ -7,9 +8,10 @@ function Projects() {
       <h1>Projects</h1>
 
       <div className={styles.grid}>
+        {/* STUDIO GHIBLI PROJECT */}
         <div className={styles.card}>
-          <h2>Studio Ghibli App &rarr;</h2>
-          <p>
+          <h2 className={styles.title}>Studio Ghibli App </h2>
+          <p className={styles.projDesc}>
             I used an API to create an app that allows you to search for your
             favourite Studio Ghibli movies.
           </p>
@@ -19,10 +21,12 @@ function Projects() {
             </a>
           </button>
         </div>
-
+        {/* HACKAPLANNER PROJECT */}
         <div className={styles.card}>
-          <h2>Hackaplanner &rarr;</h2>
-          <p>A planning app that helps you stay organised</p>
+          <h2 className={styles.title}>Hackaplanner</h2>
+          <p className={styles.projDesc}>
+            A planning app that helps you stay organised
+          </p>
 
           <button className={styles.btn}>
             <a
@@ -33,11 +37,31 @@ function Projects() {
               demo
             </a>
           </button>
-        </div>
 
+          <button className={styles.btn}>
+            <a
+              href="https://github.com/SchoolOfCode/national-project-week-coding-cats"
+              target="_blank"
+              rel="noreferrer"
+            >
+              see code
+            </a>
+          </button>
+        </div>
+        {/* RADUNO PROJECT */}
         <div className={styles.card}>
-          <h2>Raduno! &rarr;</h2>
-          <p>An event creating app</p>
+          <h2 className={styles.title}>Raduno! </h2>
+          <p className={styles.projDesc}>An event creating app</p>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              src={raduno}
+              height="300px"
+              width="390px"
+              alt="a picture of ashlie"
+            />
+          </div>
+
           <button className={styles.btn}>
             <a
               href="https://raduno-plans.netlify.app"
@@ -45,6 +69,16 @@ function Projects() {
               rel="noreferrer"
             >
               demo
+            </a>
+          </button>
+
+          <button className={styles.btn}>
+            <a
+              href="https://github.com/SchoolOfCode/final-project-front-end-hexcode"
+              target="_blank"
+              rel="noreferrer"
+            >
+              see code
             </a>
           </button>
         </div>
