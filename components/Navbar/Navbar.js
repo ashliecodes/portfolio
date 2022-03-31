06cfa2a8245d26bc24b8552fad6695e22af24aed
ashliecodes <65076082+ashliecodes@users.noreply.github.com>
2022-03-31 17:51:50 +0100

    added conditional rendering to nav bar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,42 +1,31 @@
-import Link from "next/link";
 import styles from "./navbar.module.scss";
 import Navlinks from "./Navlinks";
+import { FaBars } from "react-icons/fa";
+import { useWindowSize } from "../../hooks/useWindowSize";
 
 function Navbar() {
+  const windowSize = useWindowSize();
+  console.log(windowSize);
   return (
     <div className={styles.container}>
       <nav className={styles.nav}>
         <h1 className={styles.logo}>
           <Navlinks navLinks={"/"} name={"ashliecodes"} />
         </h1>
-
-        <div className={styles.navContainer}>
-          <Navlinks navLinks={"/about"} name={"about"} />
-          <Navlinks navLinks={"/projects"} name={"projects"} />
-          <Navlinks navLinks={"/contact"} name={"contact"} />
-        </div>
+        {windowSize.width > 730 ? (
+          <div className={styles.navContainer}>
+            <Navlinks navLinks={"/about"} name={"about"} />
+            <Navlinks navLinks={"/projects"} name={"projects"} />
+            <Navlinks navLinks={"/contact"} name={"contact"} />
+          </div>
+        ) : (
+          <div className={styles.hamburger}>
+            <FaBars />
+          </div>
+        )}
       </nav>
     </div>
   );
 }
 
 export default Navbar;
-
-{
-  /* <div className={styles.logo}>
-          <Link href={"/"}>
-            <a>&lt;ashliecodes/&gt;</a>
-          </Link>
-        </div>
-        <div className={styles.navlinks}>
-          <Link href={"/about"}>
-            <a>about</a>
-          </Link>
-          <Link href={"/projects"}>
-            <a>projects</a>
-          </Link>
-          <Link href={"/contact"}>
-            <a>contact</a>
-          </Link>
-        </div> */
-}
