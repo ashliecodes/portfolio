61a54e3ef7a75195656248603baa82545d036983
ashliecodes <65076082+ashliecodes@users.noreply.github.com>
2022-04-05 21:06:24 +0100

    project page

--- pages/contact.js
+++ pages/contact.js
@@ -3,9 +3,9 @@ import styles from "../styles/contact.module.scss";
 
 function Contact() {
   return (
-    <section className={styles.contactForm}>
-      <h2 className={styles.contactTitle}>contact:</h2>
-    </section>
+    <div className={styles.contactForm}>
+      <h2 className={styles.projectTitle}>projects:</h2>
+    </div>
   );
 }
 
--- pages/projects.js
+++ pages/projects.js
@@ -1,88 +1,124 @@
 import styles from "../styles/projects.module.scss";
 import Image from "next/image";
 import raduno from "../images/raduno.png";
+import hackerplanner from "../images/hackerplanner.png";
+import portfolio from "../images/portfolio.png";
 
 function Projects() {
   return (
     <div className={styles.container}>
-      <h2 className={styles.projectTitle}>projects:</h2>
-
-      <div className={styles.grid}>
+      <div className={styles.projectTitle}>
         <h2 className={styles.projectTitle}>projects:</h2>
-        {/* STUDIO GHIBLI PROJECT */}
-        <div className={styles.card}>
-          <h2 className={styles.title}>Studio Ghibli App </h2>
-          <p className={styles.projDesc}>
-            I used an API to create an app that allows you to search for your
-            favourite Studio Ghibli movies.
-          </p>
-          <button className={styles.btn}>
-            <a href="" target="_blank" rel="noreferrer">
-              demo
-            </a>
-          </button>
-        </div>
-        {/* HACKAPLANNER PROJECT */}
-        <div className={styles.card}>
-          <h2 className={styles.title}>Hackaplanner</h2>
-          <p className={styles.projDesc}>
-            A planning app that helps you stay organised
-          </p>
-
-          <button className={styles.btn}>
-            <a
-              href="https://hackaplanner.netlify.app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              demo
-            </a>
-          </button>
-
-          <button className={styles.btn}>
-            <a
-              href="https://github.com/SchoolOfCode/national-project-week-coding-cats"
-              target="_blank"
-              rel="noreferrer"
-            >
-              see code
-            </a>
-          </button>
-        </div>
-        {/* RADUNO PROJECT */}
-        <div className={styles.card}>
-          <h2 className={styles.title}>Raduno! </h2>
-          <p className={styles.projDesc}>An event creating app</p>
-          <div className={styles.imgContainer}>
-            <Image
-              className={styles.img}
-              src={raduno}
-              height="300px"
-              width="390px"
-              alt="a picture of ashlie"
-            />
+        <section className={styles.grid}>
+          {/* PORTFOLIO */}
+          <div className={styles.card}>
+            <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                src={portfolio}
+                height="250px"
+                width="390px"
+                alt="a picture of ashlie"
+              />
+            </div>
+            <p className={styles.projDesc}>
+              I used Next.js for my portfolio as I wanted to learn and practice
+              a new framework to develop my skills.
+            </p>
+            <button className={styles.btn}>
+              <a
+                href="https://ashliecodes.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                demo
+              </a>
+            </button>
+            <button className={styles.btn}>
+              <a
+                href="https://github.com/ashliecodes/portfolio"
+                target="_blank"
+                rel="noreferrer"
+              >
+                see code
+              </a>
+            </button>
           </div>
+          {/* HACKAPLANNER PROJECT */}
+          <div className={styles.card}>
+            <h2 className={styles.title}>Hackaplanner:</h2>
+            <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                src={hackerplanner}
+                height="300px"
+                width="390px"
+                alt="a picture of ashlie"
+              />
+            </div>
+            <p className={styles.projDesc}>
+              A planning app that helps you plan weekly hackathons at the School
+              of Code stay organised.
+            </p>
 
-          <button className={styles.btn}>
-            <a
-              href="https://raduno-plans.netlify.app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              demo
-            </a>
-          </button>
+            <button className={styles.btn}>
+              <a
+                href="https://hackaplanner.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                demo
+              </a>
+            </button>
 
-          <button className={styles.btn}>
-            <a
-              href="https://github.com/SchoolOfCode/final-project-front-end-hexcode"
-              target="_blank"
-              rel="noreferrer"
-            >
-              see code
-            </a>
-          </button>
-        </div>
+            <button className={styles.btn}>
+              <a
+                href="https://github.com/SchoolOfCode/national-project-week-coding-cats"
+                target="_blank"
+                rel="noreferrer"
+              >
+                see code
+              </a>
+            </button>
+          </div>
+          {/* RADUNO PROJECT */}
+          <div className={styles.card}>
+            <h2 className={styles.title}>Raduno!: </h2>
+
+            <div className={styles.imgContainer}>
+              <Image
+                className={styles.img}
+                src={raduno}
+                height="300px"
+                width="390px"
+                alt="a picture of ashlie"
+              />
+            </div>
+            <p className={styles.projDesc}>
+              An event planning app which allows you to catch up without the
+              fuss.
+            </p>
+            <button className={styles.btn}>
+              <a
+                href="https://raduno-plans.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                demo
+              </a>
+            </button>
+
+            <button className={styles.btn}>
+              <a
+                href="https://github.com/SchoolOfCode/final-project-front-end-hexcode"
+                target="_blank"
+                rel="noreferrer"
+              >
+                see code
+              </a>
+            </button>
+          </div>
+        </section>
       </div>
     </div>
   );
