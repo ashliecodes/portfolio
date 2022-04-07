bcad0b76853497ab4e8f5e3f0bd7e64ad951f81f
ashliecodes <65076082+ashliecodes@users.noreply.github.com>
2022-04-07 05:05:48 +0100

    more styling and trying to make things responsive

--- pages/about.js
+++ pages/about.js
@@ -15,14 +15,15 @@ function About() {
             <text className={styles.aboutDescription}>
               After working in the legal industry as a legal assistant, I
               decided that law wasn&apos;t for me. I felt very unstimulated and
-              became underwhelmed quickly... I am fun and creative (at least I
-              like to think so), so I wanted something that matched this -
-              something that challendged me whilst also being fun and creative.
-              Software development struck that balance for me!
+              underwhelmed. I am fun and creative (at least I like to think so).
+              I searched far and wide for something that allowed me to express
+              this side of myself which was hidden away for so long - something
+              that challenged me whilst also being fun and creative... Software
+              development allowed me to do just that!
               <br />
               <br />I did the Code First Girls Introduction into Web Development
               course and Introduction into Python Programming course which was
-              my first taste of coding and I wanted more (greedy I know!) This
+              my first taste of coding and I wanted more (greedy I know!). This
               led me to join the School of Code where I was selected from 1800
               applicants to train full-time for 16-weeks in order to finally
               kickstart my long awaited career in software development.
